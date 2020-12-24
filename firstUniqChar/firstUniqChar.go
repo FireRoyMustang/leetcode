14a8145b5b9fe0334173407a7765a0c406d41e58
Shen <syb@bupt.edu.cn>
2020-12-24 11:50:21 +0800

    candy

---
+++ firstUniqChar/firstUniqChar.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(firstUniqChar("helloworld"))
+}
+func firstUniqChar(s string) int {
+	length := len(s)
+	if length == 0 {
+		return -1
+	}
+	if length == 1 {
+		return 0
+	}
+	m := make(map[byte]bool, 0)
+	arr := make([]int, 26)
+	for i := 0; i < length; i++ {
+		isFirst, appear := m[s[i]]
+		if !appear {
+			m[s[i]] = true
+			arr[s[i]-'a'] = i
+		} else if isFirst {
+			m[s[i]] = false
+		}
+	}
+	ans := -1
+	for char, isFirst := range m {
+		if isFirst {
+			if ans == -1 {
+				ans = arr[char-'a']
+			} else {
+				ans = min(ans, arr[char-'a'])
+			}
+		}
+	}
+	return ans
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+func firstUniqChar2(s string) int {
+	cnt := [26]int{}
+	for _, ch := range s {
+		cnt[ch-'a']++
+	}
+	for i, ch := range s {
+		if cnt[ch-'a'] == 1 {
+			return i
+		}
+	}
+	return -1
+}
