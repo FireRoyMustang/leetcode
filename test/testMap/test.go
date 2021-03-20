31f687e3bf6363c4641ebcc368941b2604fcbcce
FireRoyMustang <syb@bupt.edu.cn>
2021-03-20 13:16:19 +0800

    2021.3.20

--- test/testMap/test.go
+++ test/testMap/test.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	slice1 := []int{1, 2, 3}
-	slice2 := make([]int, 4)
-	copy(slice2, slice1)
-	fmt.Println(slice2)
+	mp1 := make(map[int]int)
+	mp1[1]++
+	fmt.Println(mp1[1])
 }
