04499becb0f88e220dcb3f3273ceb9d22e48dc1c
FireRoyMustang <syb@bupt.edu.cn>
2021-03-21 11:32:24 +0800

    update

--- Algorithm/73_setZeroes/setZeroes.go
+++ Algorithm/73_setZeroes/setZeroes.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // 给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。
 // 请使用原地算法。
 
@@ -8,17 +10,35 @@ package main
 // 你能想出一个常数空间的解决方案吗？
 
 func main() {
-
+	matrix := [][]int{{0, 1, 2, 0},
+		{3, 4, 5, 2},
+		{1, 3, 1, 5},
+	}
+	setZeroes(matrix)
+	fmt.Println(matrix)
 }
 
 func setZeroes(matrix [][]int) {
 	m, n := len(matrix), len(matrix[0])
-	zerosRow := make(map[int]bool)
-	zerosCol := make(map[int]bool)
-	for row := 0; row < m; row++ {
-		for col := 0; col < n; col++ {
-
+	zeroRow := make(map[int]bool)
+	zeroCol := make(map[int]bool)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if matrix[i][j] == 0 {
+				zeroRow[i] = true
+				zeroCol[j] = true
 			}
 		}
 	}
+	for row := range zeroRow {
+		for i := 0; i < n; i++ {
+			matrix[row][i] = 0
+		}
+	}
+	for col := range zeroCol {
+		for i := 0; i < m; i++ {
+			matrix[i][col] = 0
+		}
+	}
+
 }
